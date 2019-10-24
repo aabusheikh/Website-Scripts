e84a54655ca91aaf1e5d090114dc082b8de2c14c
Ahmad A <1888053+bbpgrs@users.noreply.github.com>
2019-10-24 01:12:40 -0400

    Create Netflix_CtrlShiftAltD_RClick_TM.js

---
+++ Netflix_CtrlShiftAltD_RClick_TM.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         CtrlShiftAltD
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Simulate CTRL+SHIFT+ALT+D on right-click
+// @author       bbpgrs
+// @match        https://www.netflix.com/watch/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    async function CtrlShiftAltD() {
+        await sleep(5000);
+        document.body.addEventListener('contextmenu', (e) => {
+            e.preventDefault();
+
+            var evt = new KeyboardEvent('keydown', {
+                'keyCode':68, // D
+                'which':68,
+                'ctrlKey':true,
+                'altKey':true,
+                'shiftKey':true,
+                'bubbles':true,
+                'view':window
+            });
+            document.dispatchEvent(evt);;
+
+            return false;
+        }, false);
+    }
+
+    CtrlShiftAltD();
+})();
